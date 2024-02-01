5dc5406ff13754ffbcdbdd83bda664d3b46f1284
devyank <devyanknagpal2002@gmail.com>
2024-02-01 17:15:19 +0530

    deploy error in netlify resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+// @ts-ignore
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Register from './pages/Register.tsx'
 import Login from './pages/Login.tsx'
